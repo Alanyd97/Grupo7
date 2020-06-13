dff1d74bb13142b0e340531ef1318b45e13d5080
Ezequiel Falcon <exzequielfalcon@gmail.com>
2020-06-13 19:11:56 -0300

    Removed unused dependency| linkBranch GRUPO7-53

--- flyseven/src/test/java/com/metodologia/flyseven/service/EstadisticasServiceTest.java
+++ flyseven/src/test/java/com/metodologia/flyseven/service/EstadisticasServiceTest.java
@@ -1,6 +1,5 @@
 package com.metodologia.flyseven.service;
 
-import com.metodologia.flyseven.model.Viaje;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Set;
 
