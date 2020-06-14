6c43a8c0581ba3278d99e6dfffe1065de0675f66
Ezequiel Falcon <exzequielfalcon@gmail.com>
2020-06-14 14:10:55 -0300

    Se agrego soporte para levantar solo la ui y deploy a heroku

--- ui/src/app/services/vuelo.service.ts
+++ ui/src/app/services/vuelo.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VueloService {
 
-  URL = 'http://localhost:8080/vuelo'
+  URL = environment.apiUrl + '/vuelo'
 
   constructor(private httpClient: HttpClient) { }
 
